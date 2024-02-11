ab19641b93563d7de522d0213373ef9d54e5d98a
KrAvnish <avnishkr999@gmail.com>
2024-02-11 15:37:49 +0530

    first commit

---
+++ banking-app/src/main/java/com/cts/bankingapp/repository/AccountRepository.java
@@ -0,0 +1,7 @@
+package com.cts.bankingapp.repository;
+
+import com.cts.bankingapp.entity.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccountRepository extends JpaRepository<Account, Long> {
+}
